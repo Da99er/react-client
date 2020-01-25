f39282652c343f494dd00af1fae12c5ed14a25ae
Da99er <Da99er87@ya.ru>
2020-01-25 18:42:29 +0300

    refactor ssr

--- src/server.js
+++ src/server.js
@@ -3,16 +3,12 @@ import ReactDOMServer from 'react-dom/server';
 
 import App from '@root/App';
 
-global.window = {
-    IS_SERVER: true,
-    MODE: process.env.MODE,
-    addEventListener() {}, // eslint-disable-line no-empty-function
-};
-
-export default (initalState, requestUrl) => {
+const appCreator = (initalState, requestUrl) => {
 
     const element = React.createElement(App, {initalState, requestUrl}, null);
 
     return ReactDOMServer.renderToString(element);
 
 };
+
+export default appCreator;
